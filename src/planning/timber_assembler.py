573f24f7ac20cbb521907eda98597cf860cb34a9
obucklin <obucklin@gmail.com>
2023-11-21 10:39:40 +0100

    Pick N Place no collisions

--- src/planning/timber_assembler.py
+++ src/planning/timber_assembler.py
@@ -1,32 +1,29 @@
-
 import math
-from compas_rhino.conversions import RhinoPlane
 from compas_ghpython import draw_frame
-from compas_ghpython import list_to_ghtree
 from compas_fab.ghpython.components import create_id
 from compas.geometry import Frame
+from compas.geometry import Vector
 from compas_fab.robots import AttachedCollisionMesh
-from compas_rhino.geometry import RhinoMesh
+from compas_fab.robots import Robot
+from compas_fab.robots import JointConstraint
 
 class TimberAssemblyPlanner(object):
 
-    def __init__(self, robot, group = None, start_configuration = None, path_constraints = None):
+    def __init__(self, robot, group = None, start_configuration = None, path_constraints = None, planner_id = None):
         self.robot = robot
         self.group = group or self.robot.main_group_name
-        if path_constraints:
-            self.path_constraints = path_constraints
-        if not start_configuration:
-            self.current_configuration = robot.zero_configuration()
-        else:
-            self.current_configuration = start_configuration
+        self.attached_collision_meshes = []
+        self.planner_id = str(planner_id) if planner_id else "RRTConnect"
+        self.path_constraints = path_constraints if path_constraints else None
+        self.current_configuration = start_configuration if start_configuration else robot.zero_configuration()
 
-    def goal_constraints(self, plane, tolerance_position = None, tolerance_xaxis = None, tolerance_yaxis = None, tolerance_zaxis = None):
+
+    def goal_constraints(self, frame, tolerance_position = None, tolerance_xaxis = None, tolerance_yaxis = None, tolerance_zaxis = None):
         tolerance_position = tolerance_position or 0.001
         tolerance_xaxis = tolerance_xaxis or 1.0
         tolerance_yaxis = tolerance_yaxis or 1.0
         tolerance_zaxis = tolerance_zaxis or 1.0
 
-        frame = RhinoPlane.from_geometry(plane).to_compas_frame()
         tolerances_axes = [
             math.radians(tolerance_xaxis),
             math.radians(tolerance_yaxis),
@@ -34,30 +31,34 @@ class TimberAssemblyPlanner(object):
         ]
         return self.robot.constraints_from_frame(frame, tolerance_position, tolerances_axes, self.group)
             
-    def get_trajectory(self, target_frame, attached_collision_meshes = None, path_constraints = None, planner_id = None):
+    def get_trajectory(self, target_frame, linear = False, attached_collision_meshes = None, path_constraints = None, planner_id = None):
         if path_constraints:
             self.path_constraints = list(path_constraints)
-        attached_collision_meshes = list(attached_collision_meshes) if attached_collision_meshes else None
+        if attached_collision_meshes:
+            self.attached_collision_meshes.extend([AttachedCollisionMesh(mesh, 'robot11_tool0', touch_links = self.robot.get_link_names()) for mesh in attached_collision_meshes])
         planner_id = str(planner_id) if planner_id else "RRTConnect"
 
-        if (self.robot.client and self.robot.client.is_connected and goal_constraints):
+        if (self.robot.client and self.robot.client.is_connected):
             options = dict(
-                    attached_collision_meshes=self.attached_collision_meshes,
+                    attached_collision_meshes = self.attached_collision_meshes,
                     path_constraints=self.path_constraints,
                     planner_id=self.planner_id
                     )
-            goal_constraints = self.goal_constraints(target_frame)
-            trajectory = self.robot.plan_motion(goal_constraints, start_configuration=self.current_configuration, group=self.group, options = options)
+            if linear:
+                print("Linear motion through {}".format(target_frame))
+                this_trajectory = self.robot.plan_cartesian_motion(target_frame, start_configuration=self.current_configuration, group=self.group, options = options)
+            else:
+                goal_constraint = self.goal_constraints(target_frame)
+                this_trajectory = self.robot.plan_motion(goal_constraint, start_configuration=self.current_configuration, group=self.group, options = options)
 
-        return trajectory
+        return this_trajectory
 
         
-    def add_frame_trajectory_configs(self, target_frame, path_constraints = None, attach_collision_meshes = None, planner_id = None):
-        print("mesh is " + str(attach_collision_meshes))
-        trajectory = self.get_trajectory(target_frame, attached_collision_meshes=attach_collision_meshes, path_constraints=path_constraints, planner_id=planner_id)
+    def get_motion_trajectory_configs(self, target_frame, linear = False, path_constraints = None, attach_collision_meshes = None, planner_id = None):
+        motion_trajectory = self.get_trajectory(target_frame, linear, attached_collision_meshes = attach_collision_meshes, path_constraints = self.path_constraints, planner_id=planner_id)
         configs = []
-        for c in trajectory.points:
-            configs.append(self.robot.merge_group_with_full_configuration(c, trajectory.start_configuration, self.group))
+        for c in motion_trajectory.points:
+            configs.append(self.robot.merge_group_with_full_configuration(c, motion_trajectory.start_configuration, self.group))
             planes = []
             positions = []
             velocities = []
@@ -70,14 +71,17 @@ class TimberAssemblyPlanner(object):
         self.current_configuration = configs[-1]
         return configs
 
-
-
-
-
     def pick_and_place(self, target_frames, object_meshes, pickup_frame, path_constraints = None, group = None):
+        print(self.robot.get_link_names())
+        
         self.configurations = []
         self.current_configuration['robot11_joint_EA_Z'] = -4
-        self.path_constraints = path_constraints
+        self.current_configuration['robot12_joint_EA_Y'] = -12
+        if path_constraints:
+            self.path_constraints = path_constraints
+        else:
+            self.path_constraints = [JointConstraint('robot11_joint_EA_Z', -3, 1, 1, 1.0)]
+
         if group:
             self.group = group
         else:
@@ -86,47 +90,49 @@ class TimberAssemblyPlanner(object):
         self.pickup_frame = pickup_frame
         
         for index, target_frame in enumerate(target_frames):
-            group = 'robot11_eaXYZ'
-            self.pick(object_meshes[index])
-            self.place(target_frame)
+            try: 
+                self.pick(object_meshes[index])
+                self.place(target_frame, Vector(0,0,-0.5))
+            except:
+                print("sucks to suck!")
         # return outputs if you have them; here I try it for you:
         return self.configurations
     
 
-    def pick(self, mesh, pickup_frame):
+
+
+    def pick(self, mesh, pickup_frame = None):
+        print("pick")
         if pickup_frame:
             self.pickup_frame = pickup_frame
-        pickup_frame_offset = Frame(self.pickup_frame.Origin + self.pickup_frame.ZAxis * -0.5, self.pickup_frame.XAxis, self.pickup_frame.YAxis)
-        self.path_to_position(pickup_frame_offset)
-        self.linear_to_position(self.pickup_frame)        
+        pickup_frame_offset = Frame(self.pickup_frame.point - self.pickup_frame.zaxis * 0.2, self.pickup_frame.xaxis, self.pickup_frame.yaxis)
+        self.add_path_to_position(pickup_frame_offset)
+        self.add_path_to_position([pickup_frame_offset, self.pickup_frame], linear=True)        
         self.grab_part(mesh)
-        self.path_to_position(pickup_frame_offset)
+        self.add_path_to_position([self.pickup_frame, pickup_frame_offset], linear=True)
 
     def place(self, frame, approach_vector = None):
+        print("place")
         if approach_vector:
-            approach_frame = frame
-            approach_frame.point = approach_frame.point - approach_vector
-            self.path_to_position(approach_frame)
-            self.linear_to_position(frame)
+            approach_frame = Frame(frame.point - approach_vector, frame.xaxis, frame.yaxis)
+            self.add_path_to_position(approach_frame)
+            self.add_path_to_position([approach_frame, frame], linear=True)
         else:
-            self.path_to_position(frame)    
+            self.add_path_to_position(frame)    
         self.release_part()
-        retract_frame = frame
-        retract_frame.point = retract_frame.point + (retract_frame.zaxis*0.5)
-        self.retract_robot(retract_frame)
+        retract_frame = Frame(frame.point - frame.zaxis*0.5, frame.xaxis, frame.yaxis)
+        self.add_path_to_position([frame, retract_frame], linear=True)
 
 
-    def grab_part(self, mesh = None):
-        pass
-        #self.robot.append attachedCollisionMesh(mesh)
+    def grab_part(self, meshes = None):
+        self.attached_collision_meshes = None #[AttachedCollisionMesh(mesh, 'robot11_tool0', touch_links = self.robot.get_link_names()) for mesh in meshes]
 
-    def path_to_position(self, frame):
-        self.configurations.append(self.add_frame_trajectory_configs(frame))
-
-    def linear_to_position(self, frame, approach_vector):
-        frame.point = frame.point+approach_vector
-        self.configurations.append(self.add_frame_trajectory_configs(frame))
+    def add_path_to_position(self, frame, linear = False):
+        self.configurations.extend(self.get_motion_trajectory_configs(frame, linear))
 
     def release_part(self):
-        pass
+        self.attached_beam_mesh = [] 
         #self.robot.remove attachedCollisionMesh(mesh)
+
+    def attached_collision_meshes(self):
+        return [AttachedCollisionMesh(mesh) for mesh in self.attached_beam_mesh]
